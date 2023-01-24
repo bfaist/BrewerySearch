0369134d55567cc680b6c29725f804032b27e949
Bob Faist <bob.faist@gmail.com>
2023-01-23 19:39:13 -0500

    Initial Commit

---
+++ BrewerySearch/BrewerySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewerySearchApp.swift
+//  BrewerySearch
+//
+//  Created by Robert Faist on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BrewerySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
